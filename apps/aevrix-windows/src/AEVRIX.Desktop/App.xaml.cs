c0fae616d794b032c9cdb52403f2c5a71a8fb207
Zoreia85 <mvr190485@gmail.com>
2026-08-17 09:23:28 -0300

    feat(windows): gate desktop behind persisted first-run acceptance

--- apps/aevrix-windows/src/AEVRIX.Desktop/App.xaml.cs
+++ apps/aevrix-windows/src/AEVRIX.Desktop/App.xaml.cs
@@ -1,3 +1,6 @@
+using System;
+using System.IO;
+using Aevrix.Core;
 using Microsoft.UI.Xaml;
 
 namespace AEVRIX.Desktop;
@@ -12,6 +15,24 @@ public partial class App : Application
     }
 
     protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        var stateRoot = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+            "AEVRIX",
+            "UserData");
+        var firstRunStore = new FirstRunAcceptanceStore(stateRoot);
+
+        if (firstRunStore.IsAccepted())
+        {
+            OpenMainWindow();
+            return;
+        }
+
+        _window = new FirstRunWindow(firstRunStore, OpenMainWindow);
+        _window.Activate();
+    }
+
+    private void OpenMainWindow()
     {
         _window = new MainWindow();
         _window.Activate();
